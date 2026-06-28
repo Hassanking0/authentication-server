e435f45d3b150a4d5437044ba98890f7ed707fa3
Hassanking0 <hassanking1241@gmail.com>
2026-06-28 20:20:44 +0500

    feat: add dns package and configure DNS servers for MongoDB connection
    - Added dns package to package.json
    - Set custom DNS servers (8.8.8.8, 1.1.1.1) in db.ts
    - Updated MongoDB connection to use MONGO_URI from environment variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import dns from "dns"
 
 const connectDB = async (): Promise<void> => {
   try {
-    const uri = process.env.MONGO_URI;
-    if (!uri) throw new Error("MONGO_URI is not defined in .env");
 
-    const conn = await mongoose.connect('mongodb+srv://hassandev1241_db_user:vcnbqBX4BIEWkILv@cluster0.jivjpcv.mongodb.net/test?appName=Cluster0');
+    dns.setServers(['8.8.8.8', '1.1.1.1']);
+    const uri = process.env.MONGO_URI;
+    if (!uri) throw new Error("MONGO_URI is not defined");
+
+
+    const conn = await mongoose.connect(uri);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error);
